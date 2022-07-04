b9bec7ceb12db16661252eee8c0d3d7b09fd4baf
Owen Lee <hyeokjin1992@gmail.com>
2022-07-05 01:55:33 +0900

    [Title] Create main screen [Description] Create main screen

--- app/src/main/java/com/project/helpmeat/view/InitFragment.kt
+++ app/src/main/java/com/project/helpmeat/view/InitFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.content.res.AppCompatResources
@@ -14,7 +16,6 @@ import com.project.helpmeat.R
 import com.project.helpmeat.navigator.Anim
 import com.project.helpmeat.navigator.AppScreens
 import com.project.helpmeat.repository.db.UserInfo
-import com.project.helpmeat.utils.ViewUtils
 import com.project.helpmeat.view.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,6 +27,11 @@ class InitFragment : BaseFragment() {
 
     private lateinit var mInput: EditText
     private var mIsButtonActivated = false
+
+    private lateinit var mScaleUpAnimation: Animation
+    private lateinit var mScaleDownAnimation: Animation
+
+    private var mIsItemClicked = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,19 +51,26 @@ class InitFragment : BaseFragment() {
                 startButton.setImageDrawable(AppCompatResources.getDrawable(view.context, R.drawable.black_fire))
             } else {
                 mIsButtonActivated = true
-                startButton.setImageDrawable(AppCompatResources.getDrawable(view.context, R.drawable.fire))
+                startButton.setImageDrawable(AppCompatResources.getDrawable(view.context, R.drawable.fire_with_message))
             }
         }
 
+        mScaleUpAnimation = AnimationUtils.loadAnimation(context, R.anim.scale_up_large)
+        mScaleUpAnimation.fillAfter = true
+        mScaleDownAnimation = AnimationUtils.loadAnimation(context, R.anim.scale_down_large)
+        mScaleDownAnimation.fillAfter = true
+
         startButton.setOnTouchListener { v, e ->
-            v?.let {
-                if (mIsButtonActivated) {
-                    when (e?.action) {
-                        MotionEvent.ACTION_DOWN -> {
-                            ViewUtils.scaleDown(v, SCALE_RATIO)
+            if (mIsButtonActivated && v != null && e != null) {
+                when (e.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        if (!mIsItemClicked) {
+                            mIsItemClicked = true
+                            v.startAnimation(mScaleDownAnimation)
                         }
-                        MotionEvent.ACTION_UP -> {
-                            ViewUtils.scaleUp(v, SCALE_RATIO)
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        if (mIsItemClicked) {
                             mAppViewModel.updateUserInfo(UserInfo(0, mInput.text.toString()))
                             mNavigator.navigateTo(AppScreens.MAIN, Anim.SLIDE)
                         }
--- app/src/main/java/com/project/helpmeat/view/MainFragment.kt
+++ app/src/main/java/com/project/helpmeat/view/MainFragment.kt
@@ -1,17 +1,50 @@
 package com.project.helpmeat.view
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.FrameLayout
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.project.helpmeat.R
 import com.project.helpmeat.view.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainFragment : BaseFragment() {
+
+    private lateinit var mTopContainer: ConstraintLayout
+    private lateinit var mMiddleLeftContainer: LinearLayout
+    private lateinit var mMiddleRightContainer: LinearLayout
+    private lateinit var mBottomContainer: FrameLayout
+
+    private lateinit var mScaleUpAnimation: Animation
+    private lateinit var mScaleDownAnimation: Animation
+
+    @SuppressLint("ClickableViewAccessibility")
+    private val mOnTouchListener = View.OnTouchListener { v, e ->
+        if (v != null && e != null) {
+            when (e.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    if (!mIsItemClicked) {
+                        mIsItemClicked = true
+                        v.startAnimation(mScaleDownAnimation)
+                    }
+                }
+                MotionEvent.ACTION_UP -> {
+                    if (mIsItemClicked) {
+                        mIsItemClicked = false
+                        v.clearAnimation()
+                    }
+                }
+            }
+        }
+        v?.onTouchEvent(e) ?: true
+    }
+
+    private var mIsItemClicked = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,11 +57,24 @@ class MainFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val root = view.findViewById<FrameLayout>(R.id.fragment_main_content_container)
-
+        val root = view.findViewById<LinearLayout>(R.id.fragment_main_content_container)
         limitContentViewArea(root, false)
 
         setActionBarTitle("Help Meat", requireContext().getColor(R.color.heavy_pink))
+
+        mScaleUpAnimation = AnimationUtils.loadAnimation(context, R.anim.scale_up_small)
+        mScaleUpAnimation.fillAfter = true
+        mScaleDownAnimation = AnimationUtils.loadAnimation(context, R.anim.scale_down_small)
+        mScaleDownAnimation.fillAfter = true
+
+        mTopContainer = view.findViewById(R.id.fragment_main_top_container)
+        mTopContainer.setOnTouchListener(mOnTouchListener)
+        mMiddleLeftContainer = view.findViewById(R.id.fragment_main_middle_left_container)
+        mMiddleLeftContainer.setOnTouchListener(mOnTouchListener)
+        mMiddleRightContainer = view.findViewById(R.id.fragment_main_middle_right_container)
+        mMiddleRightContainer.setOnTouchListener(mOnTouchListener)
+        mBottomContainer = view.findViewById(R.id.fragment_main_bottom_container)
+        mBottomContainer.setOnTouchListener(mOnTouchListener)
     }
 
     override fun getMenuId(): Int = R.menu.main_fragment_menu
